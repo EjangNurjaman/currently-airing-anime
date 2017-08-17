7bc4f86f91d8dfd18ca1f2e2abeea52592cd71ad
Rick Lancee <rickondraw@gmail.com>
2017-08-17 21:29:13 +0200

    remove node example

--- example/node.js
+++
@@ -1,9 +0,0 @@
-import fetch from 'node-fetch'
-
-global.fetch = fetch
-
-import currentlyAiringAnime from '../currentlyAiringAnime'
-
-currentlyAiringAnime().then(({shows, next}) => {
-  console.log(shows)
-})
